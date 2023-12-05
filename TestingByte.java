034e701e45bca7599d4e147ff77f782cc208a09f
amareshaangadi07 <amaresha.xworkz@gmail.com>
2023-12-05 11:04:26 +0530

    Aish

---
+++ TestingByte.java
@@ -0,0 +1,56 @@
+public class TestingByte {
+  		public static void main(String[] args) {
+			byte age=60;
+			System.out.println(age);
+		 age=80;
+			System.out.println(age);
+
+		byte SubjectsInSemester=8;
+			System.out.println(SubjectsInSemester);
+		 SubjectsInSemester=12;
+			System.out.println(SubjectsInSemester);
+
+		byte BatteryPercentage=7;
+			System.out.println(BatteryPercentage);
+		 BatteryPercentage=70;
+			System.out.println(BatteryPercentage);
+
+		byte HourseInADay=24;
+			System.out.println(HourseInADay);
+
+		byte SemesterPercentage=80;
+			System.out.println(SemesterPercentage);
+		 SemesterPercentage=77;
+			System.out.println(SemesterPercentage);
+
+		byte IndianStates=28;
+			System.out.println(IndianStates);
+
+		byte UnionTerriteries=8;
+			System.out.println(UnionTerriteries);
+
+
+		byte Continents=7;
+			System.out.println(Continents);
+
+		byte DistrictsInKarnataka=31;
+			System.out.println(DistrictsInKarnataka);
+
+		byte DaysInMonth=30;
+			System.out.println(DaysInMonth);
+		 DaysInMonth=28;
+			System.out.println(DaysInMonth);
+		DaysInMonth=31;
+			System.out.println(DaysInMonth);
+
+
+		byte MonthsInYear=12;
+			System.out.println(MonthsInYear);
+
+		byte SpeedOfCar=90;
+			System.out.println(SpeedOfCar);
+		 SpeedOfCar=110;
+			System.out.println(SpeedOfCar);
+		
+				}
+}
