3cadb1e7a80a000113cb3cce6f6d2c9eb7097846
amareshaangadi07 <amaresha.xworkz@gmail.com>
2023-12-13 19:58:03 +0530

    Arrays

---
+++ School.java
@@ -0,0 +1,30 @@
+public class School {
+
+    public static void main(String[] args) {
+      
+	int [] classArray={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+	float[] percentage={85.15f,78.45f,65.15f,93.85f,89.26f,89.56f,65.26f,93.25f,56.26f,87.26f,87.15f,68.45f,85.15f,73.85f,99.26f,89.56f,85.26f,94.25f,56.26f,87.26f};
+	short[] admissionNumber={001,002,003,123,521,689,256,894,456,785,789,546,654,321,126,125,124,129,128,127};
+	byte[] age={(byte)12,(byte)36,(byte)6,(byte)5,(byte)12,(byte)13,(byte)15,(byte)16,(byte)12,(byte)16,(byte)12,(byte)13,(byte)18,(byte)19,(byte)17,(byte)13,(byte)12,(byte)8,(byte)9,(byte)6};
+	boolean[] pass = {true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false};
+
+	char[] alphabets = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+	double[] doublearray={6.236,7.236,66.236,78.236,67.236,47.236,86.236,87.236,66.236,77.236,96.236,87.236,56.236,72.236,6.236,7.236,6.236,7.236,6.236,7.236};
+	long[]phoneNumbers={9113026390l,9874561238l,9568741231l,9113026390l,9874561238l,9568741231l,9113026390l,9874561238l,9568741231l,9113026890l,9874561738l,9568740231l,9113016390l,9894561238l,9568747231l,9113086390l,9874561238l,9568748231l,9113026890l,9874561248l};
+for(int i =0;i<20;i++){
+        System.out.println(classArray[i]);
+	System.out.println(percentage[i]);
+	 System.out.println(admissionNumber[i]);
+	System.out.println(age[i]);
+	 System.out.println( pass[i]);
+	System.out.println(alphabets[i]);
+	 System.out.println(doublearray[i]);
+	System.out.println(phoneNumbers[i]);
+
+    }
+
+   }
+
+    
+}
